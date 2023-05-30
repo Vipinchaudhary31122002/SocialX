84769c2ce5823215cf7f829c4faee110cdf24bcc
VipinChaudhary <vipinchaudhary31122002@gmail.com>
2023-05-30 10:40:23 +0530

    video completed upto 2:17 and now i have to work on uploading post

--- SocialX/core/views.py
+++ SocialX/core/views.py
@@ -70,4 +70,29 @@ def logout(request):
 # view for the settings page
 @login_required(login_url='signin')
 def settings(request):
-    return render(request, 'setting.html')
+    user_profile = Profile.objects.get(user=request.user)
+
+    # after taking input from the user saving the changed data in the database
+    if request.method == 'POST':
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+            
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+            
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        return redirect('settings')
+
+    return render(request, 'setting.html', {'user_profile': user_profile})
